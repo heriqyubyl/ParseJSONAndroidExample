bd053f117de7946049041dde25a08934f3733f69
Heri Qyubyl <heritkom0004@gmail.com>
2021-05-03 10:07:05 +0700

    example

---
+++ app/src/main/java/com/example/parsejsonandroidexample/Cell.kt
@@ -0,0 +1,8 @@
+package com.example.parsejsonandroidexample
+
+data class Cell(
+        var employeeId: String,
+        var employeeName: String,
+        var employeeSalary: String,
+        var employeeAge: String
+)
